507ca9ce504fd18015a54b8680ce9dcf62ceef5d
mun-jihye <m84422583@gmail.com>
2023-01-02 18:38:51 +0900

    Apply Material Ui

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,38 +1,22 @@
 import React from "react";
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
     render(){
         //실제로 그려지는 내용이 여기에 담김
         return(
             //내부가 2개 이상일 경우 <div> 같은 태그로 감싸야 오류가 발생하지 않음.
-            <div> 
-                <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt='profile'/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 export default Customer;
